91f54acf1af3568d844f2c12329c80aa28703c91
rafredyramirez <ra.fredyramirez@gmail.com>
2021-02-14 23:22:40 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import HelloWorld from './components/HelloWord'
+import Header from './components/Header';
+
+ReactDOM.render(<Header />, document.getElementById('app'));
